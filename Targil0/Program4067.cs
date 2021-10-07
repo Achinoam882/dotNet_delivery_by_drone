10370055d35fec01925621ac3292ec332a534574
Achinoam <Achinoam@DESKTOP-M2VSBJG>
2021-10-07 20:01:58 +0300

    Initial code+Welcome functions

---
+++ Targil0/Program4067.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4067();
+            Welcome4931();
+            Console.ReadKey();
+        }
+        static partial void Welcome4931();
+        private static void Welcome4067()
+        {
+            Console.WriteLine("Enter your name: ");
+            String str = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", str);
+        }
+    }
+}
