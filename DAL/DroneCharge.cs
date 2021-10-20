cdd80c90af99f4e9708cab436aa07d191667b629
Achinoam <Achinoam@DESKTOP-M2VSBJG>
2021-10-20 13:29:05 +0300

    ALL

--- DAL/DroneCharge.cs
+++ DAL/DroneCharge.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace DAL
+namespace IDAL
 {
-    public struct DroneCharge
+    namespace DO
     {
+        public struct DroneCharge
+        {
+            public int DroneId { get; set; }
+            public int StationId { get; set; }
+            public override string ToString()
+            {
+                return string.Format("Drone Id is:{0}\t  StationId is:{1}\t"  , DroneId, StationId);
+            }
+
+        }
     }
 }
--- DAL/Parcel.cs
+++ DAL/Parcel.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace DAL
+namespace IDAL
 {
-    public struct Parcel
+    namespace DO
     {
-        public int id;
-        public int senderld;
-        public int targetId;
-        public  WeightCategories Weight;
+        public struct Parcel
+        {
+            public int Id { get; set; }
 
+            public int SenderId { get; set; }
 
+            public int TargetId { get; set; }
 
+            public WeightCategories Weight { get; set; }
+
+            public Priorities Priority { get; set; }
+
+            public int DroneId { get; set; }
+
+            public DateTime Scheduled { get; set; }
+
+            public DateTime PickUp { get; set; }
+
+            public DateTime Delivered { get; set; }
+
+            public DateTime Requested { get; set; }
+            public override string ToString()
+            {
+                return string.Format("Id is:{0}\t Sender Id is:{1}\t  Target Id is:{2}\t " +
+                    " Drone Weight is:{3}\t  Priority is:{4}\t  Drone Id is:{5}\t  Scheduled time is:{6}\t " +
+                    " PickUp time is:{7}\t  Delivered time is:{8}\t  Requested time is:\t"  , Id, SenderId,
+                    TargetId, Weight, Priority, DroneId, Scheduled, PickUp, Delivered, Requested);
+            }
+
+        }
     }
 }
