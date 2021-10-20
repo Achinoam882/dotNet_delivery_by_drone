e20c6a1a235dc0252671650e51bf46f427a76e39
Achinoam <Achinoam@DESKTOP-M2VSBJG>
2021-10-20 12:59:41 +0300

    main

---
+++ DAL/BaseStation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+   public struct BaseStation
+    {
+        public int id;
+        public string Name { get; set; }
+        public double longitute;
+        public double latitude;
+        public int chargeslots;
+    }
+}
---
+++ DAL/Parcel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public struct Parcel
+    {
+        public int id;
+        public int senderld;
+        public int targetId;
+        public  WeightCategories Weight;
+
+
+
+    }
+}
